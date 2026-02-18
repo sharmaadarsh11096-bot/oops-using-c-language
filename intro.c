b83b4386b0e21e2e0efce3c222d36ce0a1b39862
Adarsh Sharma <sharmaadarsh11096@gmail.com>
2026-02-18 16:34:21 +0530

    First commit

---
+++ intro.c
@@ -0,0 +1,116 @@
+// wap to add two numbers
+// c lang code
+
+// int main()
+// {
+//     int x,y,add=0; 
+//     printf("enta the value of x and y :");
+//     scanf("%d %d,"x, y);
+//     add = x+y;
+//     printf("the sum is %d, add"; 
+//         // printf(print formated)
+//     return 0;)
+// }
+
+// // c++
+// #include<isostream>
+// int main()
+// {
+//     int x,y, add=0;
+//     count << "enter the value ");
+//     (int >>x >>y;
+//     add = x+y;
+//     count <<"the sum:"<< add;
+//     return 0;
+
+// } 
+// develop a mod  whese the input is given by user about students details which include student id, name markes of 5 subject.
+// if parcentage >90 then 'o'
+// parcentage >80 &<=90 'A'
+// parcentage >70 &<=80 'B'
+// parcentage >60 &<=70 'C'
+// parcentage >50 &<=60 'D'
+// parcentage >40 &<=50 'P'
+// else fail.
+// class student 
+// {
+//     int id, marks=0;
+//     int marks [5];
+//     char name [30];
+//     public;
+//     void input ();
+//     void cal ();
+//     void print ();
+    
+//     };
+//       void student :: input ()
+//       {
+//         count <<"enter student's detail:";
+//         count <<"in enter student's id:";
+//         count >> student id;
+//         count>>"enter student name:";
+//         cin >> name;
+//       }
+// LOOP type-
+// 1 entry control 
+// 1.1 for loop for(initilization, condition, inc/dec)
+// 1.2 while loop (condition)
+// 2 exit control 
+// 2.1 do while loop     
+
+// void student :: cal ()
+// {
+//     for (int i=0; i<5; i++)
+//     {t marks = marks [i];= t marks = t marks + marks[i];
+//     }
+//     pa= t_marks / 50
+// }
+// void student :: print (
+//     { 
+//         count <<"id"<="\t name"<<" \t tstu marks " << " \t
+//       percentage
+//     }
+//     )
+
+
+// --------------------------------------------------------------------------------- puneet sir DSA -------------------------------------------------------------------
+// #include <studio.h>
+// int main()
+// { 
+//     char a[] = "Programming 2020";
+//     printf ("%s",a+a[3]-a[5]);
+//  return 0; 
+// }
+
+
+// int main()
+// {
+//     char a[10];
+//     char *b = "string";
+//     int length =strlen(b);
+
+//     for( int i= 0; i<length; i++){
+
+//         a[i] = b[length-i];
+//     }
+//     printf("%s" , a);
+
+// return 0;}
+
+//--------------------------------------------------------------- c++ --------------------------------------------------------------------------
+
+constractor => is a special function whuch is used to inclised the object of its classs.... the constructor is invocked when ever an object of its accostatid class is created...
+
+function => 
+           two type => 
+                       standured / spacial
+
+
+
+  
+
+
+
+
+
+
